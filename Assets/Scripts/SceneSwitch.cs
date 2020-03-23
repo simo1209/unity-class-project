db6a0777bb83301cbeceed2f2c367fb36dd6dd4e
Simeon Georgiev <simeongeorgiev09@gmail.com>
2020-03-23 16:36:00 +0200

    Added scenes for the mini games

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class SceneSwitch : MonoBehaviour
+{
+
+    public string SceneName;
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
