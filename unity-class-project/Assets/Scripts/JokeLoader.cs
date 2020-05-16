2c14ef4c7bc6f0f79391ce92efdbe4fef31d0920
Simeon Georgiev <simeongeorgiev09@gmail.com>
2020-05-16 12:28:01 +0300

    Added roulette

--- unity-class-project/Assets/Scripts/JokeLoader.cs
+++ unity-class-project/Assets/Scripts/JokeLoader.cs
@@ -2,58 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Networking;
-
-
-public class joke_class {
-    public string Joke;
-
-    public joke_class(string Joke) {
-        this.Joke = Joke;
-    }
-    public string get_joke() {
-        return this.Joke;
-    }
-
-    public void format_joke() {
-        Joke.Trim('\\');
-    }
-}
 
 public class JokeLoader : MonoBehaviour
 {
 
     public Text joke;
-
-    public IEnumerator get_random_joke()
-    {
-        string joke_url_template = "http://127.0.0.1:5000/jokes/{0}";
-        int num = Random.Range(0, 97);
-        string joke_url = string.Format(joke_url_template, num);
-        UnityWebRequest www = UnityWebRequest.Get(joke_url);
-
-        yield return www.SendWebRequest();
-        //Debug.LogWarning(www.downloadHandler.text);
-
-        if (www.isNetworkError || www.isHttpError)
-        {
-            Debug.Log(www.error);
-        }
-        else
-        {
-            // Show results as text
-            joke_class requested_joke = JsonUtility.FromJson<joke_class>(www.downloadHandler.text);
-            requested_joke.format_joke();
-            joke.text = requested_joke.Joke;
-        }
-    }
+    public Text pucnhline;
 
     // Start is called before the first frame update
     public void Start()
     {
-        StartCoroutine(get_random_joke());
-    }
 
+        TextAsset jokes_asset = (TextAsset)Resources.Load("dad_jokes"); // Loads a text file with jokes
+        string[] jokes = jokes_asset.text.Split('\n'); // Splits the asset to jokes
+
+        string[] random_joke = jokes[Random.Range(0,jokes.Length-1)].Split(';'); // Every joke is split in content and punchline with ';'
+        joke.text = random_joke[0];
+        pucnhline.text = random_joke[1];
+
+    }
 
     // Update is called once per frame
     void Update()
---
+++ unity-class-project/Assets/Scripts/RouletteScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteScript : MonoBehaviour
+{
+    public float rotateSpeed;
+    private bool rotate = false;
+
+    public void OnClick()
+    {
+        rotateSpeed = 600f;
+        rotate = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotate)
+        {
+            transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+            rotateSpeed -= 2;
+            if(rotateSpeed < 0)
+            {
+                rotate = false;
+            }
+        }
+    }
+}
